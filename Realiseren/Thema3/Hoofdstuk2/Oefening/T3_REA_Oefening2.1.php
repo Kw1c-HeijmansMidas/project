313012948fda8178b0ba8ae5eb539728cbae9d4b
MikeHuijberts <mike.huijberts@edu-kw1c.nl>
2022-02-14 17:46:00 +0100

    T3_REA_Oefening2.1.php Opdracht 2.1

--- Realiseren/Thema3/Hoofdstuk2/Oefening/T3_REA_Oefening2.1.php
+++ Realiseren/Thema3/Hoofdstuk2/Oefening/T3_REA_Oefening2.1.php
@@ -1,1 +1,63 @@
 <?php
+/**
+ * User: Mike Huijberts
+ * Date: 9-2-2021
+ * File: T3_REA_Oefening1.1.php
+ */
+?>
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <title>
+            <?php
+                echo 'Realiseren instructies thema 3 en 4.';
+            ?>
+        </title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="style.css" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <h1>
+                <?php
+                    echo "Uitwerkingen van PHP-oefeningen";
+                ?>
+            </h1>
+        </header>
+        <?php
+            //Hier gaan we in het volgende hoofdstuk het menu toevoegen.
+        ?>
+        <main id="wrapper">
+            <nav>
+                <h2>Thema 3</h2>
+                <ul><li><h3 class="kop">Hoofdstuk 2</h3></li>
+                    <ul>
+                        <li><a href="/REA_T3T4/Realiseren/Thema3/Hoofdstuk1/Oefening/T3_REA_Oefening1.1.php">Oefening 1.1</a></li>
+                        <li><a href="/REA_T3T4/Realiseren/Thema3/Hoofdstuk1/Oefening/T3_REA_Oefening1.2.php">Oefening 1.2</a></li>
+                    </ul>
+                </ul>
+            </nav>
+            <section>
+                <h2>
+                    Uitwerking
+                </h2>
+
+                <!-- Plaats je code / uitwerking hieronder -->
+                <p>
+                    <?php
+                        echo "<p>PHP is een server-side scripting taal gemaakt voor het gebruik van web. PHP heeft direct contact met de server in tegenstelling
+                                 tot bijvoorbeeld Javascript die contact heeft met de guest. Een van de reden voor het gebruik van PHP is het gebruik van een database zodat je bij een grote web winkel niet alle producten in de index hoeft te zetten.
+                     </p>
+                        <h2>Git&Github</h2>
+                        <p>
+                            Github is ervoor om op te slaan wat je hebt gedaan met commit dat je een aanvraag stuurt en push dat je het erin zet.
+                            Als je een clone maakt van de repository maak je een kopie van alles wat je in Github hebt staan.
+                        </p>";
+                    ?>
+                </p>
+            </section>
+        </main>
+    </body>
+</html>
+
