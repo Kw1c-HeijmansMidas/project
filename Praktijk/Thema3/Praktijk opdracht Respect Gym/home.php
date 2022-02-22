2d48533d4ea49cfd8d1d04141a988e4415664879
MikeHuijberts <mike.huijberts@edu-kw1c.nl>
2022-02-22 09:00:09 +0100

    Praktijk opdracht Respect GYm 1

---
+++ Praktijk/Thema3/Praktijk opdracht Respect Gym/home.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * User: Mike Huijberts
+ * Date: 9-2-2021
+ * File: T3_REA_Oefening1.1.php
+ */
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>
+        <?php
+        echo 'Realiseren instructies thema 3 en 4.';
+        ?>
+    </title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./stylesheet/home.css" rel="stylesheet">
+</head>
+<body>
+<header>
+    <h1>
+        <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">FAQ</a></li>
+            </ul>
+            </ul>
+        </nav>
+    </h1>
+</header>
+</body>
+</html>
+
