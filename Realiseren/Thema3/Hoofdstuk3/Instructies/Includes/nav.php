6437e741d886c5ba28c92969d569cc9f34552215
MikeHuijberts <mike.huijberts@edu-kw1c.nl>
2022-03-11 13:58:52 +0100

    Realiseren opdracht 4.1 klaar

--- Realiseren/Thema3/Hoofdstuk3/Instructies/Includes/nav.php
+++ Realiseren/Thema3/Hoofdstuk3/Instructies/Includes/nav.php
@@ -1,10 +1,10 @@
-<nav>
-    <h2>Thema 3</h2>
-    <ul><li><h3 class="kop">Hoofdstuk 2</h3></li>
-        <ul>
-            <li><a href="/Realiseren/Thema3/Hoofdstuk1/Oefening/T3_REA_Oefening1.1.php">Oefening 1.1</a></li>
-            <li><a href="/Realiseren/Thema3/Hoofdstuk2/Oefening/T3_REA_Oefening2.1.php">Oefening 2.1</a></li>
-            <li><a href="/Realiseren/Thema3/Hoofdstuk3/Oefening/T3_REA_Oefening3.1.php">Oefening 3.1</a></li>
+    <nav>
+        <h2>Thema 3</h2>
+        <ul><li><h3 class="kop">Hoofdstuk 2</h3></li>
+            <ul>
+                <li><a href="/Realiseren/Thema3/Hoofdstuk1/Oefening/T3_REA_Oefening1.1.php">Oefening 1.1</a></li>
+                <li><a href="/Realiseren/Thema3/Hoofdstuk2/Oefening/T3_REA_Oefening2.1.php">Oefening 2.1</a></li>
+                <li><a href="/Realiseren/Thema3/Hoofdstuk3/Oefening/T3_REA_Oefening3.1.php">Oefening 3.1</a></li>
+            </ul>
         </ul>
-    </ul>
-</nav>
+    </nav>
---
+++ Realiseren/Thema3/Hoofdstuk4/T3_REA_Oefening_4_2.php
@@ -0,0 +1,45 @@
+<?php
+    include "Includes/variabelen.php"
+    ?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>
+        <?php
+        echo 'Realiseren instructies thema 3 en 4.';
+        ?>
+    </title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../Instructies/stylesheet/style.css" rel="stylesheet">
+</head>
+<body>
+<?php
+    include "Includes/header.php"
+?>
+<?php
+//Hier gaan we in het volgende hoofdstuk het menu toevoegen.
+?>
+<main id="wrapper">
+    <?php
+        include "Includes/nav.php"
+    ?>
+    <section>
+        <h2>
+            Uitwerking
+        </h2>
+
+        <!-- Plaats je code / uitwerking hieronder -->
+        <p>
+            <?php
+            echo "Op deze plek komt de uitwerking van de oefening te staan";
+            ?>
+        </p>
+    </section>
+</main>
+<?php
+    include "Includes/footer.php"
+?>
+</body>
+</html>
+
