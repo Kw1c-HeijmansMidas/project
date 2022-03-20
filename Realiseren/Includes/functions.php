9686e5a2b91e3e52619b114c97d84d55afda5491
MikeHuijberts <mike.huijberts@edu-kw1c.nl>
2022-03-20 23:39:25 +0100

    4.3 snap de rest niet vraag om uitleg

--- Realiseren/Includes/functions.php
+++ Realiseren/Includes/functions.php
@@ -27,25 +27,25 @@ $date = date ("U");
 ?>
 
 <?php
-            function CalculateInterest($saved, $savedUp, $procent)
-            {
-                $total = $savedUp * $procent;
-                $counter = 0;
-                while($saved < $savedUp)
-                {
-
-                    $total = $total * $procent;
-
-                    $counter = $counter + 1;
-                }
-
-
-            }
-            CalculateInterest(2500, 1500, 1.06);
-
-echo "Een leuk tweedehands autootje kost 2500 euro. Ik heb 1500 euro gespaard. Ik zet het geld op de bank en krijg daarop een waanzinnige rente van 6% per maand! Ik ga met een while-loop uitrekenen hoeveel maanden ik moet wachten om de 2500 euro te betalen.
-                maanden moet ik nog betalen. <br>";
-
-echo $total;
+//            function CalculateInterest($saved, $savedUp, $procent)
+//            {
+//                $total = $savedUp * $procent;
+//                $counter = 0;
+//                while($saved < $savedUp)
+//                {
+//
+//                    $total = $total * $procent;
+//
+//                    $counter = $counter + 1;
+//                }
+//
+//
+//            }
+//            CalculateInterest(2500, 1500, 1.06);
+//
+//echo "Een leuk tweedehands autootje kost 2500 euro. Ik heb 1500 euro gespaard. Ik zet het geld op de bank en krijg daarop een waanzinnige rente van 6% per maand! Ik ga met een while-loop uitrekenen hoeveel maanden ik moet wachten om de 2500 euro te betalen.
+//                maanden moet ik nog betalen. <br>";
+//
+//echo $total;
 
 ?>
