9b3ebc1374d261e5733745886d86cc4d6d865277
MikeHuijberts <mike.huijberts@edu-kw1c.nl>
2022-04-08 11:57:03 +0200

    Opdracht 6.3 af

--- Realiseren/Thema3/Hoofdstuk6/Opdrachten/T3_REA_Oefening6.3.php
+++ Realiseren/Thema3/Hoofdstuk6/Opdrachten/T3_REA_Oefening6.3.php
@@ -1,6 +1,12 @@
 <?php
-include "../../../././Includes/variabelen.php"
+include "../../../././Includes/variabelen.php";
+session_start();
+if (isset($_SESSION['user1']) == false || ($_SESSION['user2']) == false ) {
+    $_SESSION['user1'] = 0;
+    $_SESSION['user2'] = 0;
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="nl">
 <head>
@@ -31,42 +37,55 @@ include "../../../././Includes/header.php"
 
         <!-- Plaats je code / uitwerking hieronder -->
 
-            <form id="gameFrm" method="get" action="T3_REA_Oefening6.3.php">
-                <div class="float"><input type="radio"
-                                          onchange="document.getElementById('gameFrm').submit();" name="keuze"
-                                          value="steen"><img src="images/steen.png" width="50px" height="50px"></div>
-                <div class="float"><input type="radio"
-                                          onchange="document.getElementById('gameFrm').submit();" name="keuze"
-                                          value="papier"><img src="images/papier.png" width="50px" height="50px"></div>
-                <div class="float"><input type="radio"
-                                          onchange="document.getElementById('gameFrm').submit();" name="keuze"
-                                          value="schaar"><img src="images/Scissor-PNG-HD-Images.png" width="50px" height="50px"></div>
-            </form>
-            <?php
-            if (isset($_GET['keuze']))
-            {
-            echo "Jij koos: <img src='images/{$_GET['keuze']}.png' width='50px' height='50px'>";
+        <form id="gameFrm" method="get" action="T3_REA_Oefening6.3.php">
+            <div class="float"><input type="radio"
+                                      onchange="document.getElementById('gameFrm').submit();" name="keuze"
+                                      value="steen"><img src="images/steen.png" width="50px" height="50px"></div>
+            <div class="float"><input type="radio"
+                                      onchange="document.getElementById('gameFrm').submit();" name="keuze"
+                                      value="papier"><img src="images/papier.png" width="50px" height="50px"></div>
+            <div class="float"><input type="radio"
+                                      onchange="document.getElementById('gameFrm').submit();" name="keuze"
+                                      value="schaar"><img src="images/schaar.png" width="50px"
+                                                          height="50px"></div>
+        </form>
+
+
+        <?php
+
+        if (isset($_GET["keuze"])) {
+            echo "Jij koos: <img src='images/{$_GET["keuze"]}.png' width='50px' height='50px'>";
             //keuze computer?
-            }
+
             //Wat kiest de computer
             //Random wordt er een getal tussen 0 en 2 gekozen
-             $opties = array("steen","papier","schaar");
-             $computerkeuzegetal = rand(0,2);
-             $computerkeuze = $opties[$computerkeuzegetal];
+            $opties = array("steen", "papier", "schaar");
+            $computerkeuzegetal = rand(0, 2);
+            $computerkeuze = $opties[$computerkeuzegetal];
             echo "&nbsp;&nbsp;De computer koos: <img src='images/{$computerkeuze}.png' width='50px' height='50px'>";
-            ?>
-            <?php
-            $count1 = 0;
-            $count2 = 0;
 
-            if ($_GET['keuze'] == "papier" && $computerkeuze == "steen" || gebruiker steen && computer schaar || gebruiker schaar && computer papier)
 
-            ?>
-            <br>
-            <br>
-            <input type="submit" value="Versturen">
+            if ($_GET["keuze"] == "papier" && $computerkeuze == "steen" || $_GET["keuze"] == "steen" && $computerkeuze == "schaar" || $_GET["keuze"] == "schaar" && $computerkeuze == "papier") {
+                $_SESSION['user1'] += 1;
+            } elseif ($_GET["keuze"] == "steen" && $computerkeuze == "papier" || $_GET["keuze"] == "schaar" && $computerkeuze == "steen" || $_GET["keuze"] == "papier" && $computerkeuze == "schaar") {
+                $_SESSION['user2'] += 1;
+            }
+            echo "<br>" . "Jou score is " . $_SESSION['user1'] . " de computers score is " . $_SESSION['user2'];
+            if ($_SESSION['user1'] == 5)
+            {
 
-        </form>
+                echo "<br>" . "Jij hebt gewonnen";
+                session_destroy();
+
+
+            }
+            elseif ($_SESSION['user2'] == 5)
+            {
+                echo "<br>" . "De computer heeft gewonnen";
+                session_destroy();
+            }
+        }
+        ?>
 
 </main>
 <?php
