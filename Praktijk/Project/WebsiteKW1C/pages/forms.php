2096b3b4ef2b324b9e7f343ada6477152cf46a80
Midas <midas.heijmans@edu-kw1c.nl>
2022-04-13 12:21:58 +0200

    results

---
+++ Praktijk/Project/WebsiteKW1C/pages/forms.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * User: Midas Heijmans
+ * Date: 14-03-22
+ * File: Project thema 3
+ */
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>
+        <?php
+        echo 'Realiseren instructies thema 3 en 4.';
+        ?>
+    </title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../styles/homekw1c.css" rel="stylesheet">
+    <link href="../styles/forms.css" rel="stylesheet">
+</head>
+<body>
+<h1 id="title">
+    Vragenlijst:
+</h1>
+<img src="../images/Screenshot%202022-04-13%20120735.jpg" alt="redline">
+<p>
+    hoi
+</p>
+</body>
+</html>
---
+++ Praktijk/Project/WebsiteKW1C/pages/results.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * User: Midas Heijmans
+ * Date: 14-03-22
+ * File: results file
+ */
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>
+
+    </title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../styles/homekw1c.css" rel="stylesheet">
+</head>
+<body>
+
+</body>
+</html>
