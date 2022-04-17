2793ef76a55ee7bcda57ae1211d91c66f9ee1ac8
MikeHuijberts <mike.huijberts@edu-kw1c.nl>
2022-04-17 18:06:48 +0200

    Praktijk

--- Praktijk/Project/WebsiteKW1C/pages/homepagekw1c.php
+++ Praktijk/Project/WebsiteKW1C/pages/homepagekw1c.php
@@ -8,62 +8,85 @@
 <link href="../styles/homekw1c.css" rel="stylesheet">
 
 <html lang="nl">
-<header>
+<body>
+    <header>
+
+        <?php
+        include "../includes/header.php"
+        ?>
+
+
+    </header>
+
 
     <?php
-    include "../includes/header.php"
+    //Hier gaan we in het volgende hoofdstuk het menu toevoegen.
     ?>
+    <main>
+        <img src="../images/achtergrond.png" id="background">
+
+    </main>
+    <article>
+        <h1 id="titel">ICT-ACADEMIE</h1>
+            <!--Maakt rood witte lijn aan-->
+            <img src="../images/Screenshot%202022-04-13%20120735.jpg" id="lijn">
+
+        <h3 class="middenstuk" id="kop1">Algemeen</h3>
+            <p class="middenstuk" id="tekst1">
+                Een goede ICT’er is gedreven om te leren en is bereid om kennis te delen met anderen. Dat vinden ze bij het bedrijfsleven. Verder moet je wat willen bereiken, plezier hebben in wat je doet en goed in een team kunnen werken. Tijdens je studie aan de ICT-Academie krijg je hiermee te maken. Dat gebeurt zoveel mogelijk in projectvormen: leren werken en werkend leren in klein groepsverband, met opdrachten uit de praktijk. Teamwork dus. Belangrijk is verder de adviesraad, die bestaat uit vertegenwoordigers van vooraanstaande bedrijven uit de regio.
+            </p>
+        <div id="iframe1">
+            <h3 class="middenstuk" id="kop2">Video-impressie</h3>
+                <iframe width="640" height="360" src="https://www.youtube.com/embed/P5E7u8-CRsQ?wmode=transparent" allowfullscreen="" style="height: 360px;"></iframe>
+        </div>
+        <div id="iframe2">
+            <h3 class="middenstuk" id="kop3">Domeinvideo Techniek</h3>
+                <iframe width="640" height="360" src="https://www.youtube.com/embed/qa4lm5l_4K0" allowfullscreen="" style="height: 360px;"></iframe>
+        </div>
+        <div id="iframe3">
+            <h3 class="middenstuk" id="kop2">Awardgenomineerde 2021: Leon Klerkx</h3>
+            <p>Leon Klerkx is in 2021 genomineerd voor de Koning Willem I College Awards. Hij won de Award voor Bijzonder Verhaal.</p>
+                <iframe width="640" height="360" src="https://www.youtube.com/embed/wD3flexFmSs" allowfullscreen="" style="height: 360px;"></iframe>
+
+        </div>
+    </article>
+
+    <aside>
+        <h3 class="aside" id="info">Informatie</h3>
+            <p>
+                Download de brochure van deze afdeling:
+            </p>
+
+                <a href="/media/afdeling/folders/811 ICT Folderlijn aangepast.pdf" id="cphInhoud_aDown" title="Download de brochure" class="aDown bgRood" target="_blank">
+                    <i class="fas fa-download"></i>&nbsp;Brochure in PDF</a>
 
 
-</header>
-<body>
-
-<?php
-//Hier gaan we in het volgende hoofdstuk het menu toevoegen.
-?>
-<main>
-    <img src="../images/achtergrond.png" id="background">
-
-</main>
-<article>
-    <h1 id="titel">ICT-ACADEMIE</h1>
-        <!--Maakt rood witte lijn aan-->
-        <img src="../images/Screenshot%202022-04-13%20120735.jpg" id="lijn">
-
-    <h3 class="middenstuk" id="kop1">Algemeen</h3>
-        <p class="middenstuk" id="tekst1">
-            Een goede ICT’er is gedreven om te leren en is bereid om kennis te delen met anderen. Dat vinden ze bij het bedrijfsleven. Verder moet je wat willen bereiken, plezier hebben in wat je doet en goed in een team kunnen werken. Tijdens je studie aan de ICT-Academie krijg je hiermee te maken. Dat gebeurt zoveel mogelijk in projectvormen: leren werken en werkend leren in klein groepsverband, met opdrachten uit de praktijk. Teamwork dus. Belangrijk is verder de adviesraad, die bestaat uit vertegenwoordigers van vooraanstaande bedrijven uit de regio.
-        </p>
-    <h3 class="middenstuk" id="kop2">Video-impressie</h3>
-    <iframe width="640" height="360" src="https://www.youtube.com/embed/P5E7u8-CRsQ?wmode=transparent" allowfullscreen="" style="height: 360px;"></iframe>
-
-    <h3 class="middenstuk" id="kop3">Domeinvideo Techniek</h3>
-
-    <h3 class="middenstuk" id="kop2">Awardgenomineerde 2021: Leon Klerkx</h3>
-</article>
-
-<aside>
-    <h3 class="aside" id="info">Informatie</h3>
-        <p>
-            Download de brochure van deze afdeling:
-        </p>
-
-            <a href="/media/afdeling/folders/811 ICT Folderlijn aangepast.pdf" id="cphInhoud_aDown" title="Download de brochure" class="aDown bgRood" target="_blank">
-                <i class="fas fa-download"></i>&nbsp;Brochure in PDF</a>
+        <h3 class="aside" id="contact">Contact</h3>
+        <a href="tel:0736249600" title="Bel het Studenten Succes Centrum"><i class="fas fa-phone"></i>&nbsp;&nbsp;073 624 9 600</a>
+        <a href="mailto:info@kw1c.nl" title="Stuur een e-mail naar het Studenten Succes Centrum"><i class="far fa-envelope"></i>&nbsp;&nbsp;info@kw1c.nl</a>
 
 
-    <h3 class="aside" id="contact">Contact</h3>
-    <a href="tel:0736249600" title="Bel het Studenten Succes Centrum"><i class="fas fa-phone"></i>&nbsp;&nbsp;073 624 9 600</a>
-    <a href="mailto:info@kw1c.nl" title="Stuur een e-mail naar het Studenten Succes Centrum"><i class="far fa-envelope"></i>&nbsp;&nbsp;info@kw1c.nl</a>
+        <h3 class="aside" id="vraag">Heb je vragen? Wij geven jou advies:</h3>
+        <a href="/gesprek-met-studieadviseur" title="Afspraak maken voor een gesprek met een studieadviseur">
+            Gesprek met een studie­adviseur</a>
+        <a href="/stel-je-vraag" title="Stel je vraag met een online formulier">Stel je
+            vraag online</a>
+    </aside>
+    <footer>
+        <div id="dSocial">
+            <a href="https://www.facebook.com/KoningWillemICollege" target="_blank" title="Bezoek ons op Facebook">
+                <i class="facebook"></i></a>
+            <a href="https://twitter.com/KW1C" target="_blank" title="Volg ons op Twitter">
+                <i class="twitter"></i></a>
+            <a href="http://instagram.com/kw1c" target="_blank" title="Volg ons op Instagram">
+                <i class="instagram"></i></a>
+            <a href="https://www.linkedin.com/company/koning-willem-i-college" target="_blank" title="Volg ons op LinkedIn">
+                <i class="linkedin"></i></a>
+            <a href="http://www.youtube.com/KoningWillemICollege" target="_blank" title="Bekijk ons op Youtube">
+                <i class="youtube"></i></a>
 
-
-    <h3 class="aside" id="vraag">Heb je vragen? Wij geven jou advies:</h3>
-    <a href="/gesprek-met-studieadviseur" title="Afspraak maken voor een gesprek met een studieadviseur">
-        Gesprek met een studie­adviseur</a>
-    <a href="/stel-je-vraag" title="Stel je vraag met een online formulier">Stel je
-        vraag online</a>
-</aside>
-
+        </div>
+    </footer>
 
 
 
